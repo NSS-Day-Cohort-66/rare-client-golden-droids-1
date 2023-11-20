1fbbf63b0f4d5740b088c4dd50088cb65c08c7ed
Sylvia T <sylviarosemarie@gmail.com>
2023-11-20 16:05:31 -0600

    god greaking blcess just merge

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,22 +1,22 @@
-import { useRef } from "react"
-import { Link } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
-import { registerUser } from "../../managers/AuthManager"
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../../managers/AuthManager";
 
-export const Register = ({setToken}) => {
-  const firstName = useRef()
-  const lastName = useRef()
-  const email = useRef()
-  const username = useRef()
-  const bio = useRef()
-  const password = useRef()
-  const verifyPassword = useRef()
-  const passwordDialog = useRef()
-  const navigate = useNavigate()
+export const Register = ({ setToken }) => {
+  const firstName = useRef();
+  const lastName = useRef();
+  const email = useRef();
+  const username = useRef();
+  const bio = useRef();
+  const password = useRef();
+  const verifyPassword = useRef();
+  const passwordDialog = useRef();
+  const navigate = useNavigate();
 
   const handleRegister = (e) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+
     if (password.current.value === verifyPassword.current.value) {
       const newUser = {
         username: username.current.value,
@@ -24,25 +24,24 @@ export const Register = ({setToken}) => {
         last_name: lastName.current.value,
         email: email.current.value,
         password: password.current.value,
-        bio: bio.current.value
-      }
+        bio: bio.current.value,
+      };
 
-      registerUser(newUser)
-        .then(res => {
-          if ("valid" in res && res.valid) {
-            setToken(res.token)
-            navigate("/")
-          }
-        })
+      registerUser(newUser).then((res) => {
+        if ("valid" in res && res.valid) {
+          setToken(res.token);
+          navigate("/");
+        }
+      });
     } else {
-      passwordDialog.current.showModal()
+      passwordDialog.current.showModal();
     }
-  }
+  };
 
   return (
     <section className="columns is-centered">
       <form className="column is-two-thirds" onSubmit={handleRegister}>
-      <h1 className="title">Rare Publishing</h1>
+        <h1 className="title">Rare Publishing</h1>
         <p className="subtitle">Create an account</p>
         <div className="field">
           <label className="label">First Name</label>
@@ -77,13 +76,23 @@ export const Register = ({setToken}) => {
           <div className="field-body">
             <div className="field">
               <p className="control is-expanded">
-                <input className="input" type="password" placeholder="Password" ref={password} />
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="Password"
+                  ref={password}
+                />
               </p>
             </div>
 
             <div className="field">
               <p className="control is-expanded">
-                <input className="input" type="password" placeholder="Verify Password" ref={verifyPassword} />
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="Verify Password"
+                  ref={verifyPassword}
+                />
               </p>
             </div>
           </div>
@@ -92,20 +101,31 @@ export const Register = ({setToken}) => {
         <div className="field">
           <label className="label">Bio</label>
           <div className="control">
-            <textarea className="textarea" placeholder="Tell us about yourself..." ref={bio}></textarea>
+            <textarea
+              className="textarea"
+              placeholder="Tell us about yourself..."
+              ref={bio}
+            ></textarea>
           </div>
         </div>
 
         <div className="field is-grouped">
           <div className="control">
-            <button className="button is-link" type="submit">Submit</button>
+            <button
+              className="button is-link"
+              type="submit"
+              onClick={handleRegister}
+            >
+              Submit
+            </button>
           </div>
           <div className="control">
-            <Link to="/login" className="button is-link is-light">Cancel</Link>
+            <Link to="/login" className="button is-link is-light">
+              Cancel
+            </Link>
           </div>
         </div>
-
       </form>
     </section>
-  )
-}
+  );
+};
