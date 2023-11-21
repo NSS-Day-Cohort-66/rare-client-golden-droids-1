77982a90aa7af4617d2febfc69d072229a4ead0f
Tricia Swift <tricia.swift8@gmail.com>
2023-11-21 10:17:54 -0600

    wip

--- src/managers/AuthManager.js
+++ src/managers/AuthManager.js
@@ -13,7 +13,7 @@ export const loginUser = (user) => {
 };
 
 export const registerUser = (newUser) => {
-  return fetch("http://localhost:8088/register", {
+  return fetch("http://localhost:8000/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
