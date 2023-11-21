5f3e541125971aa92540e94fee9d700e4c4145e4
Zach D'Amico <zachdamico90@gmail.com>
2023-11-21 11:35:07 -0600

    staging to check tp pr

--- src/components/tags/TagList.js
+++ src/components/tags/TagList.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllTags } from "../../managers/TagManager";
 import "./Tag.css";
+import { Link } from "react-router-dom";
 
 export const TagList = ({ token, staff }) => {
   const [tags, setTags] = useState([]);
@@ -36,6 +37,9 @@ export const TagList = ({ token, staff }) => {
     <article>
       <h2>Tags</h2>
       <div className="tags--container">{displayTags()}</div>
+      <Link to="/create-tag" className="create-tag-button">
+        Create New Tag
+      </Link>
     </article>
   );
 };
