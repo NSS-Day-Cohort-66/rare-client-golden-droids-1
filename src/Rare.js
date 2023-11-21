2ee038a20ff2708e5112ab9d595677b63e6a4568
Tricia Swift <tricia.swift8@gmail.com>
2023-11-21 13:51:03 -0600

    Fixed refresh error

--- src/Rare.js
+++ src/Rare.js
@@ -4,26 +4,17 @@ import { NavBar } from "./components/nav/NavBar";
 
 export const Rare = () => {
   const [token, setTokenState] = useState(localStorage.getItem("auth_token"));
-  const [staff, setStaffState] = useState(false);
+  const staff = localStorage.getItem("staff");
 
   const setToken = (newToken) => {
     localStorage.setItem("auth_token", newToken);
     setTokenState(newToken);
   };
 
-  const setStaff = (boolean) => {
-    setStaffState(boolean);
-  };
-
   return (
     <>
       <NavBar token={token} setToken={setToken} />
-      <ApplicationViews
-        token={token}
-        setToken={setToken}
-        staff={staff}
-        setStaff={setStaff}
-      />
+      <ApplicationViews token={token} setToken={setToken} staff={staff} />
     </>
   );
 };
